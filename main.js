aff4ab6960acf04f1f2692c49e9177eecdb45afb
gimg97409 <gimg97409@gmail.com>
2026-04-03 12:37:18 +0000

    Update project files

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-// Add JS here
+const generateBtn = document.getElementById('generate-btn');
+const lottoNumbersContainer = document.getElementById('lotto-numbers');
+
+generateBtn.addEventListener('click', () => {
+    lottoNumbersContainer.innerHTML = '';
+    const numbers = new Set();
+
+    while(numbers.size < 6) {
+        const randomNumber = Math.floor(Math.random() * 45) + 1;
+        numbers.add(randomNumber);
+    }
+
+    numbers.forEach((number, index) => {
+        const circle = document.createElement('div');
+        circle.classList.add('number-circle');
+        circle.textContent = number;
+        circle.style.animationDelay = `${index * 0.1}s`;
+        lottoNumbersContainer.appendChild(circle);
+    });
+});
